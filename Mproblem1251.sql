103a534ac8f8001e7d5ae7c4e32145187fcee293
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-06-26 00:05:55 +0530

    Update Mproblem1251.sql

--- Mproblem1251.sql
+++ Mproblem1251.sql
@@ -32,10 +32,9 @@
 
 
 
--- # Write your MySQL query statement below
-SELECT DISTINCT p.product_id, ROUND(SUM(p.price*u.units)/SUM(u.units),2) AS average_price
-FROM Prices p
-INNER JOIN UnitsSold u
-ON p.product_id = u.product_id
-WHERE u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id
+-- Write your MySQL query statement below
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id 
+AND u.purchase_date BETWEEN start_date AND end_date
+group by product_id
