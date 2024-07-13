1a72d1d9e07532b229c2e0ba512212c25c1fee2e
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-07-13 07:44:12 +0000

    easy

---
+++ problem610.sql
@@ -0,0 +1,23 @@
+-- Table: Triangle
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | x           | int  |
+-- | y           | int  |
+-- | z           | int  |
+-- +-------------+------+
+-- In SQL, (x, y, z) is the primary key column for this table.
+-- Each row of this table contains the lengths of three line segments.
+ 
+
+-- Report for every three line segments whether they can form a triangle.
+
+-- Return the result table in any order.
+
+
+
+-- # Write your MySQL query statement below
+SELECT x, y, z,
+CASE WHEN x+y>z AND y+z>x AND z+x>y THEN 'Yes' ELSE 'No' END AS triangle 
+FROM Triangle
